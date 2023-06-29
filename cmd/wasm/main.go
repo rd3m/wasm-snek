62a1da931387081d4157c1793367094b4f99442d
Riley <riley@neonjungle.studio>
2023-06-29 10:56:48 +1000

    init canvas

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"syscall/js"
+)
 
 func main() {
-	fmt.Println("Hello, WebAssembly!")
+	// Init Canvas stuff
+	doc := js.Global().Get("document")
+	canvasEl := doc.Call("getElementById", "canvas")
+	width := doc.Get("body").Get("clientWidth").Float()
+	height := doc.Get("body").Get("clientHeight").Float()
+	canvasEl.Set("width", width)
+	canvasEl.Set("height", height)
+	canvasEl.Set("style", "border: 1px solid black;")
+	// ctx := canvasEl.Call("getContext", "2d")
 }
